8bf4aaabf47cae203b989d181698c3f9027b08e8
Keval Patel <kevalptl8097@gmail.com>
2023-02-04 19:43:14 +0530

    elevator-request create view updated

--- elevatorapp/drfviews.py
+++ elevatorapp/drfviews.py
@@ -4,11 +4,11 @@ from rest_framework import viewsets, mixins, status
 from django.shortcuts import get_object_or_404
 from .serializer import ElevatorSerializer, ElevatorRequestSerializer
 from .models import *
+from .background import elevator_task, door_task
+from background_task.models import Task
 
 class ElevatorViewSet(mixins.CreateModelMixin, 
                       viewsets.GenericViewSet):
-    queryset = Elevator.objects.all()
-    serializer_class = ElevatorSerializer
 
     def create(self, request, *args, **kwargs):
         no_of_elevator = request.data.get('no_of_elevator')
@@ -24,8 +24,16 @@ class ElevatorViewSet(mixins.CreateModelMixin,
 class ElevatorRequestViewSet(mixins.CreateModelMixin,
                              mixins.RetrieveModelMixin,
                              viewsets.GenericViewSet):
-    queryset = ElevatorRequest.objects.all()
-    serializer_class = ElevatorRequestSerializer
+    def create(self, request, *args, **kwargs):
+        elevator_no = request.data.get('elevator_no')
+        destination_floor = request.data.get('destination_floor')
+        serializer = ElevatorRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(elevator_no=elevator_no, destination_floor=destination_floor) 
+        tasks = Task.objects.filter(verbose_name="elevator_task_elevator{}".format(elevator_no))
+        if len(tasks) == 0:
+            elevator_task(elevator_no, verbose_name="elevator_task_elevator{}".format(elevator_no))
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     def retrieve(self, request, pk=None, *args, **kwargs):
         queryset = ElevatorRequest.objects.filter(elevator_no=pk)
         serializer = ElevatorRequestSerializer(queryset, many=True)
